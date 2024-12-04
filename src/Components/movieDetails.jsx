689af03add46640c147df282c81e506f1bda35a3
Patricia <patricia.badji89@gmail.com>
2024-12-04 16:59:16 +0100

    Update movieDetails.jsx

--- src/Components/movieDetails.jsx
+++ src/Components/movieDetails.jsx
@@ -1,5 +1,43 @@
+import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+
 const MovieDetails = () => {
-  return "ciaociao";
+  const params = useParams();
+
+  const url =
+    "http://www.omdbapi.com/?i=tt3896198&apikey=d901781a&s=Harry Potter";
+
+  const [movie, setMovie] = useState([]);
+
+  useEffect(() => {
+    getTvShowData();
+  }, []);
+
+  const getTvShowData = () => {
+    fetch(url)
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error();
+        }
+      })
+      .then((movie) => {
+        console.log(movie);
+        setMovie(movie.Search);
+      })
+      .catch((err) => {
+        console.log("Errore", err);
+      });
+  };
+
+  const findTheRightMovie = () => {
+    movie.find((movie) => {
+      return movie.imdbID.toString() === params.movieId;
+    });
+
+    console.log(findTheRightMovie);
+  };
 };
 
 export default MovieDetails;
